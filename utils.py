324727a255da180d2a11d2acd0925d178f89866c
saehan-choi <gmlssns5859@naver.com>
2021-09-15 10:43:56 +0900

    utils

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def intersction_over_union(pred_boxes,label_boxes,box_format="corners"):
         label_x2 = label_boxes[...,0:1] + label_boxes[2:3] / 2
         label_y2 = label_boxes[...,1:2] + label_boxes[3:4] / 2
 
-# 영상에서는 if box_format 이던데 안되면 여기 볼 것
+    # 영상에서는 if box_format 이던데 안되면 여기 볼 것
     elif box_format == "corners"
         pred_x1 = pred_boxes[...,0:1]  # maintain for tensor if you don't do it, tensor will be gone
         # this result is (N,1) but pred_boxes[...,0] result is (N)
@@ -48,3 +48,34 @@ def intersction_over_union(pred_boxes,label_boxes,box_format="corners"):
     label_area = abs((label_x2 - label_x1) * (label_y2 - label_y1))
 
     return intersection / (pred_area + label_area - intersection)
+
+def non-max-suppression(bboxes, iou_threshold ,threshold, box_format="corners"):
+    
+    assert type(bboxes) == list
+    # if not bboxes is list it's gonna error
+    bboxes = [box for box in bboxes if box[1] > threshold]
+    # threshold -> prob_threshold
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes_after_nms = []
+
+    # ex) 
+    # bboxes = [[1,0.9,...],[2,0.9,...],[1,0.8,...],[2,0.8,...]...]
+    # after 1
+    # bboxes = [[2,0.9,...],[2,0.8,...]...]
+    while bboxes:
+        chosen_box = bboxes.pop(0)
+
+        bboxes = [
+        box for box in bboxes
+        if box[0] != chosen_box[0]
+        or
+        intersction_over_union(
+            torch.tensor(bboxes[2:]),
+            torch.tensor(chosen_box[2:]),
+            box_format=box_format
+        ) < iou_threshhold
+        ]
+
+        bboxes_after_nms.append(chosen_box)
+
+    return bboxes_after_nms
